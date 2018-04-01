3aac0e9aec7d94532e7a6e6ea3761bd655a816a5
yura.breza <yura.breza@gmail.com>
2018-04-01 11:42:40 +0300

    added : filters fragment

--- app/src/main/java/com/hack/kind/lvivbikemap/app/di/BuilderModule.kt
+++ app/src/main/java/com/hack/kind/lvivbikemap/app/di/BuilderModule.kt
@@ -1,8 +1,6 @@
 package com.hack.kind.lvivbikemap.app.di
 
 
-import com.hack.kind.lvivbikemap.MainActivityModule
-import com.hack.kind.lvivbikemap.MainActivityScope
 import com.hack.kind.lvivbikemap.MapActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
